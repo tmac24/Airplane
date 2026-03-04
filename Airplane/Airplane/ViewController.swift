27e3156d227f676a6ea4995fc51a3a356f36c524
sunt <727924283@qq.com>
2026-03-04 15:40:45 +0800

    初始化项目

---
+++ Airplane/Airplane/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Airplane
+//
+//  Created by sunt on 2026/3/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
